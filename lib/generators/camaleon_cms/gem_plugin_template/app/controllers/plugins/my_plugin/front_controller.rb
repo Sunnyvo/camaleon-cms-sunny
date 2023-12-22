b95308b28bd3cc8be0bca5a9d17e92050b8b2211
Sunnyvo <votanphu.2101@gmail.com>
2023-12-22 20:32:26 +0700

    update code

--- lib/generators/camaleon_cms/gem_plugin_template/app/controllers/plugins/my_plugin/front_controller.rb
+++ lib/generators/camaleon_cms/gem_plugin_template/app/controllers/plugins/my_plugin/front_controller.rb
@@ -1,7 +1,6 @@
 module Plugins
   module PluginClass
     class FrontController < CamaleonCms::Apps::PluginsFrontController
-      skip_before_action :verify_authenticity_token, only: [:your_email_sending_action]
       include Plugins::PluginClass::MainHelper
       def index
         # actions for frontend module
